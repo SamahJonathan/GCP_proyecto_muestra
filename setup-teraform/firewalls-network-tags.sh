e1a3d02c6aff47f8bcfce26b27f501ea59a9e9b6
samahjonathan <jona.samah@gmail.com>
2026-08-12 12:10:40 -0400

    pate 6 firewalls

---
+++ setup-teraform/firewalls-network-tags.sh
@@ -0,0 +1,1 @@
+gcloud compute firewall-rules create allow-ssh-custom --direction=INGRESS --priority=1000 --network=mi-vpc-curso --action=ALLOW --rules=tcp:22 --source-ranges=0.0.0.0/0 --target-tags=web-server 
